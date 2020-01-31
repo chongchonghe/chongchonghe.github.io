fba4c4953af3891cbea83dab1ebe06bf88f58e39
ChongChong He <he.cc.phy@gmail.com>
2020-01-30 20:12:32 -0500

    update cv

--- files/cv/auto/cv.el
+++ files/cv/auto/cv.el
@@ -4,13 +4,16 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("resume" "letterpaper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "left=0.75in" "top=1in" "right=0.75in" "bottom=0.6in")))
+                     '(("geometry" "left=0.75in" "top=1in" "right=0.75in" "bottom=0.6in") ("inputenc" "utf8") ("fontenc" "T1")))
    (TeX-run-style-hooks
     "latex2e"
     "resume"
     "resume10"
     "enumitem"
-    "geometry")
+    "geometry"
+    "inputenc"
+    "fontenc"
+    "stix")
    (TeX-add-symbols
     '("itab" 1)
     '("tab" 1)
